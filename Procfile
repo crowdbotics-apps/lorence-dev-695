10b6d5ee4956a26e94a181934188fc63a2460d0d
Crowdbotics <team@crowdbotics.com>
2019-02-21 18:48:26 +0000

    initial commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: waitress-serve --port=$PORT backend.lorence_dev_695.wsgi:application
+release: ./backend/release-tasks.sh
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "app_name" {
+  description = "Unique name of the app"
+  type = "string"
+  default = "lorence-dev-695"
+}
+
+variable "custom_domain" {
+  description = "Custom domain name (optional)"
+  type = "string"
+  default = ""
+}
